64516503348c4aa8f15393106be7856857bf9d58
ClementMadiot <clementmadiot09@gmail.com>
2024-10-02 11:22:14 +0200

    fetch quotes api

--- Projects/Random_quote/script.js
+++ Projects/Random_quote/script.js
@@ -1,1 +1,32 @@
-import './styles.scss'
+const text = document.getElementById("text");
+const authorQuote = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+
+const API_URL = "https://api.api-ninjas.com/v1/quotes";
+
+const fetchQuotes = async () => {
+  try {
+    const res = await fetch(API_URL, {
+      headers: {
+        "X-Api-Key": "7AGZkb1lj0c/bfLwtC0SwA==wUjdmWzNKoI0TpMD",
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    const data = await res.json();
+    displayFetch(data)
+  } catch (err) {
+    console.error(`There was an error: ${err}`);
+  }
+};
+fetchQuotes();
+
+const displayFetch = (data) => {
+  const { quote, author } = data[0]
+  console.log(quote)
+  text.textContent = quote
+  authorQuote.textContent = author
+}
+
+newQuoteBtn.addEventListener("click", fetchQuotes)
