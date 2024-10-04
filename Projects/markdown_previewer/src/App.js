e5fd9471a55232b7fbdb3337ea0cb2e8cc268207
ClementMadiot <clementmadiot09@gmail.com>
2024-10-04 10:37:59 +0200

    fix img

--- Projects/markdown_previewer/src/App.js
+++ Projects/markdown_previewer/src/App.js
@@ -1,51 +1,53 @@
 import "./App.scss";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import remarkGfm from "remark-gfm";
 
 const markdown = `
 # Welcome to my React Markdown Previewer!
 
-        ## This is a sub-heading...
-        ### And here's some other cool stuff:
+## This is a sub-heading...
+
+### And here's some other cool stuff:
         
         Heres some code, \`<div></div>\`, between 2 backticks.
         
-        \`\`\`
-        // this is multi-line code:
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
         
-        function anotherExample(firstLine, lastLine) {
-          if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-            return multiLineCode;
-          }
-        }
-        \`\`\`
-        
-        You can also make text **bold**... whoa!
-        Or _italic_.
-        Or... wait for it... **_both!_**
-        And feel free to go crazy ~~crossing stuff out~~.
-        
-        There's also [links](https://www.freecodecamp.org), and
-        > Block Quotes!
-        
-        And if you want to get really crazy, even tables:
-        
-        Wild Header | Crazy Header | Another Header?
-        ------------ | ------------- | -------------
-        Your content can | be here, and it | can be here....
-        And here. | Okay. | I think we get it.
-        
-        - And of course there are lists.
-          - Some are bulleted.
-             - With different indentation levels.
-                - That look like this.
-        
-        
-        1. And there are numbered lists too.
-        1. Use just 1s if you want!
-        1. And last but not least, let's not forget embedded images:
-        
-        ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+    
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+    
+And if you want to get really crazy, even tables:
+    
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
         
 `;
 
@@ -85,8 +87,30 @@ function App() {
             <i className="fa-solid fa-up-right-and-down-left-from-center hidden"></i>
           </div>
         </div>
-        <article id="preview"></article>
-        <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />,
+        <article id="preview">
+          <ReactMarkdown
+            children={markdown}
+            remarkPlugins={[remarkGfm]}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, "")}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  />
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          />
+          ,
+        </article>
       </article>
     </main>
   );
