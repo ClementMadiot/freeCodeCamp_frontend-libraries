651c44c9e84c4e40b2c1f8a87385cda270b87c0c
ClementMadiot <clementmadiot09@gmail.com>
2024-10-04 10:10:15 +0200

    add ReactMarkdown

--- Projects/markdown_previewer/src/App.js
+++ Projects/markdown_previewer/src/App.js
@@ -1,15 +1,93 @@
-import './App.scss';
-import Previewer from './components/Previewer';
-import Textarea from './components/Textarea';
+import "./App.scss";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
+
+const markdown = `
+# Welcome to my React Markdown Previewer!
+
+        ## This is a sub-heading...
+        ### And here's some other cool stuff:
+        
+        Heres some code, \`<div></div>\`, between 2 backticks.
+        
+        \`\`\`
+        // this is multi-line code:
+        
+        function anotherExample(firstLine, lastLine) {
+          if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+            return multiLineCode;
+          }
+        }
+        \`\`\`
+        
+        You can also make text **bold**... whoa!
+        Or _italic_.
+        Or... wait for it... **_both!_**
+        And feel free to go crazy ~~crossing stuff out~~.
+        
+        There's also [links](https://www.freecodecamp.org), and
+        > Block Quotes!
+        
+        And if you want to get really crazy, even tables:
+        
+        Wild Header | Crazy Header | Another Header?
+        ------------ | ------------- | -------------
+        Your content can | be here, and it | can be here....
+        And here. | Okay. | I think we get it.
+        
+        - And of course there are lists.
+          - Some are bulleted.
+             - With different indentation levels.
+                - That look like this.
+        
+        
+        1. And there are numbered lists too.
+        1. Use just 1s if you want!
+        1. And last but not least, let's not forget embedded images:
+        
+        ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+        
+`;
 
 function App() {
   return (
     <main>
-      <div class="grid-background">
-        <div class="gradient"></div>
+      <div className="grid-background">
+        <div className="gradient"></div>
       </div>
-      <Textarea/>
-      <Previewer/>
+      <article className="textarea-content">
+        <div className="label-content">
+          <div className="label-start">
+            <i className="fa-brands fa-free-code-camp"></i>
+            <label htmlFor="editor">Editor</label>
+          </div>
+          <div className="btn-popUp">
+            <i className="fa-solid fa-maximize"></i>
+            <i className="fa-solid fa-up-right-and-down-left-from-center hidden"></i>
+          </div>
+        </div>
+        <textarea
+          name="editor"
+          id="editor"
+          type="text"
+          defaultValue={markdown}
+        ></textarea>
+      </article>
+
+      <article className="preview-content">
+        <div className="label-content">
+          <div className="label-start">
+            <i className="fa-brands fa-free-code-camp"></i>
+            <label htmlFor="editor">Previewer</label>
+          </div>
+          <div className="btn-popUp">
+            <i className="fa-solid fa-maximize"></i>
+            <i className="fa-solid fa-up-right-and-down-left-from-center hidden"></i>
+          </div>
+        </div>
+        <article id="preview"></article>
+        <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />,
+      </article>
     </main>
   );
 }
