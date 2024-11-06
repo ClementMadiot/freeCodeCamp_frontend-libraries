fff360bed20b8352fcba07e0057ec58806d4d44c
ClementMadiot <clementmadiot09@gmail.com>
2024-11-06 16:22:33 +0100

    fix decimal error

--- Projects/4_Calculator/src/App.tsx
+++ Projects/4_Calculator/src/App.tsx
@@ -5,7 +5,7 @@ import Screen from "./components/Screen";
 import Button from "./components/Button";
 
 function App() {
-  const [userValue, setUserValue] = useState("");
+  const [userValue, setUserValue] = useState("0");
   const [reset, setreset] = useState(true);
 
   if (userValue === "00") {
@@ -14,61 +14,46 @@ function App() {
   }
 
   // if (reset === true && val === "*" || val === "/" || val === "+" || val === "-") {
-    //   alert(`not allow to start with ${val}`);
-    //   return;
-    // }
+  //   alert(`not allow to start with ${val}`);
+  //   return;
+  // }
 
   const handleClick = (val: string) => {
-    // Add zero if decimal is click and val is empty 
-      if (reset && val === ".") {
-        setUserValue(userValue + "0.");
-        setreset(false)
-        return;
-      }
-      // Delete a zero if number is higher
-      if (userValue === "0" && val > "0" || /[+\-*/]$/.test(userValue) && val > "0") {
-        setUserValue(val);
-        return;
-      }
-
-      // if (/[+\-*/]$/.test(userValue) && val === "0" && !userValue.endsWith(".")) {
-      //   return;
-      // }
-      
-
-      // Not allow multiple decimal 
-    if (userValue.endsWith(".") && val === ".") {
-      setUserValue(userValue);
+    // Delete a zero if number is higher
+    if (userValue === "0" && val > "0") {
+      setUserValue(val);
       return;
     }
+
+    // Not allow multiple decimal
     const lastNumber = userValue.split(/[+\-*/]/).pop();
-    if (lastNumber && /[+\-*/.]/.test(lastNumber) && val === ".") {
+    if (lastNumber && /[.]/.test(lastNumber) && val === ".") {
       return;
     }
-
 
     setreset(false);
     setUserValue(userValue + val);
   };
 
-  const handleEqual = (val: string) => {
-    if (userValue === "" && val === "=") {
+  const handleEqual = () => {
+    if (userValue === "0") {
       alert(`not allow to start with =`);
       return;
     }
-    setUserValue(math.evaluate(userValue));
+    const result = math.evaluate(userValue);
+    setUserValue(result.toString());
   };
 
   const handleClear = () => {
-    setreset(true);
-    setUserValue("");
+    setreset(reset);
+    setUserValue("0");
   };
 
   return (
     <section className="flex justify-center text-center flex-col">
       <h1 className="my-6 ">Calculator</h1>
       <article className="bg-black p-2 border border-gold min-w-[340px] m-auto">
-        <Screen reset={reset} display={userValue} />
+        <Screen display={userValue} />
         <Button
           handleClick={handleClick}
           handleClear={handleClear}
--- Projects/4_Calculator/src/components/Screen.tsx
+++ Projects/4_Calculator/src/components/Screen.tsx
@@ -1,14 +1,15 @@
 type DisplayProps = {
-  reset: boolean;
-  display: string
-}
-function Screen({ reset, display }: DisplayProps) {
+  display: string;
+};
+function Screen({ display }: DisplayProps) {
   return (
-    <div id="display" className="bg-black text-whiteSmoke w-full h-12 text-end text-3xl">
-      {reset ? "0": false}
+    <div
+      id="display"
+      className="bg-black text-whiteSmoke w-full h-12 text-end text-3xl"
+    >
       {display}
     </div>
-  )
+  );
 }
 
-export default Screen
+export default Screen;
