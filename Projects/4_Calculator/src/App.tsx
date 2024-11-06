2c9f8438e7f98a031178346ae69f3f38279c2818
ClementMadiot <clementmadiot09@gmail.com>
2024-11-06 15:42:15 +0100

    not allow multiply decimal. zero

--- Projects/4_Calculator/src/App.tsx
+++ Projects/4_Calculator/src/App.tsx
@@ -6,19 +6,61 @@ import Button from "./components/Button";
 
 function App() {
   const [userValue, setUserValue] = useState("");
-  const [reset, setreset] = useState(true)
-  
+  const [reset, setreset] = useState(true);
+
+  if (userValue === "00") {
+    console.error("not allow to start with multiple zero");
+    return setUserValue("0");
+  }
+
+  // if (reset === true && val === "*" || val === "/" || val === "+" || val === "-") {
+    //   alert(`not allow to start with ${val}`);
+    //   return;
+    // }
+
   const handleClick = (val: string) => {
-    setreset(false)
+    // Add zero if decimal is click and val is empty 
+      if (reset && val === ".") {
+        setUserValue(userValue + "0.");
+        setreset(false)
+        return;
+      }
+      // Delete a zero if number is higher
+      if (userValue === "0" && val > "0" || /[+\-*/]$/.test(userValue) && val > "0") {
+        setUserValue(val);
+        return;
+      }
+
+      // if (/[+\-*/]$/.test(userValue) && val === "0" && !userValue.endsWith(".")) {
+      //   return;
+      // }
+      
+
+      // Not allow multiple decimal 
+    if (userValue.endsWith(".") && val === ".") {
+      setUserValue(userValue);
+      return;
+    }
+    const lastNumber = userValue.split(/[+\-*/]/).pop();
+    if (lastNumber && /[+\-*/.]/.test(lastNumber) && val === ".") {
+      return;
+    }
+
+
+    setreset(false);
     setUserValue(userValue + val);
   };
 
-  const handleEqual = () => {
+  const handleEqual = (val: string) => {
+    if (userValue === "" && val === "=") {
+      alert(`not allow to start with =`);
+      return;
+    }
     setUserValue(math.evaluate(userValue));
   };
 
   const handleClear = () => {
-    setreset(true)
+    setreset(true);
     setUserValue("");
   };
 
@@ -27,7 +69,11 @@ function App() {
       <h1 className="my-6 ">Calculator</h1>
       <article className="bg-black p-2 border border-gold min-w-[340px] m-auto">
         <Screen reset={reset} display={userValue} />
-        <Button handleClick={handleClick} handleClear={handleClear} handleEqual={handleEqual} />
+        <Button
+          handleClick={handleClick}
+          handleClear={handleClear}
+          handleEqual={handleEqual}
+        />
       </article>
       <p className="my-6 leading-loose">
         Designed and Coded By
--- Projects/4_Calculator/src/components/Screen.tsx
+++ Projects/4_Calculator/src/components/Screen.tsx
@@ -5,7 +5,7 @@ type DisplayProps = {
 function Screen({ reset, display }: DisplayProps) {
   return (
     <div id="display" className="bg-black text-whiteSmoke w-full h-12 text-end text-3xl">
-      {reset ? "0": null}
+      {reset ? "0": false}
       {display}
     </div>
   )
