0acc0745d5e1d6ba171c007b448d9d4aae07b00a
ClementMadiot <clementmadiot09@gmail.com>
2024-11-12 09:53:25 +0100

    update app

--- Projects/4_Calculator/src/App.tsx
+++ Projects/4_Calculator/src/App.tsx
@@ -5,10 +5,6 @@ import Button from "./components/Button";
 const operators = ["AC", "/", "*", "+", "-", "="];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const operators = ["AC", "/", "*", "+", "-", "="];
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const App = () => {
   const [input, setInput] = useState("0");
   const [output, setOutput] = useState("");
